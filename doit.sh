e211e8063c88b1ef566217a73716038a740c7ed2
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-07-10 11:13:08 -0400

    updating workflow

--- doit.sh
+++ doit.sh
@@ -5,4 +5,7 @@
 #  echo "delete"
 #fi
 
-az deployment group show -g fortinet-secure-cloud-blueprints -n fortinet-secure-cloud-blueprints
+#az deployment group show -g fortinet-secure-cloud-blueprints -n fortinet-secure-cloud-blueprints
+
+`az group exists -n fortinet-secure-cloud-blueprint` && az deployment group delete -g fortinet-secure-cloud-blueprint -n fortinet-secure-cloud-blueprint || true
+`az group exists -n fortinet-secure-cloud-blueprint` && az group delete --yes --name fortinet-secure-cloud-blueprint || true
